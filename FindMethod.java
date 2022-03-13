919c0695a37b9519126e20f9abb123a91d3f7a51
AbdulRazak <94792119+AbdulRazak4M@users.noreply.github.com>
2022-03-13 08:38:55 +0530

    added some  additional information
    added comments so it helps to easy identification and understanding of programs

--- FindMethod.java
+++ FindMethod.java
@@ -1,16 +1,17 @@
 import java.lang.reflect.*;
 public class FindMethod
 {
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args) throws Exception // might thorw classNotFound
 	{
-		int count = 0;
-		Class c = Class.forName("java.util.List");
-		Method[] m = c.getDeclaredMethods();
+		int count = 0; 
+		// Load the class
+		Class c = Class.forName("java.util.List"); // Class Name you want to check
+		Method[] m = c.getDeclaredMethods(); // to get mathods name
 		for(Method m1 : m)
 		{
 			count++;
 			System.out.println(m1.getName());
 		}
-		System.out.println("Totla Methods" + count);
+		System.out.println("Total Methods" + count);
 	}
 }
